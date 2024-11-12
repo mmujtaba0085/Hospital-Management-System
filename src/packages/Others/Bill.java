ab884f297b2f11a5c235db37bc4ee9ee2ade9790
Zirwah <zirwahkhalil201070@gmail.com>
2024-11-13 00:01:30 +0500

    Modified Patient and various other files

---
+++ src/packages/Others/Bill.java
@@ -0,0 +1,20 @@
+package packages.Others;
+
+public class Bill {
+    int amount;
+    Bill(){
+        amount=0;
+    }
+    Bill(int amount){
+        this.amount=amount;
+    }
+    public void payBill(){
+
+    }
+    void generateBill(){
+
+    }
+    public void displayBill(){
+
+    }
+}
--- src/packages/Patient/Patient.java
+++ src/packages/Patient/Patient.java
@@ -1,24 +1,44 @@
 package packages.Patient;
 
+import packages.Others.Bill;
+
 public class Patient {
     int ID;
     String name;
     String email;
     long phoneNumber;
+    String address;
+    String record;
+    Bill bill;
     Patient(){
         ID=0;
         name="";
         email="";
         phoneNumber=0;
+        address="";
+        record="";
     }
-    Patient(int ID, String name, String email, long phoneNumber){
+    Patient(int ID, String name, String email, long phoneNumber, String address){
         this.ID=ID;
         this.name=name;
         this.email=email;
         this.phoneNumber=phoneNumber;
+        this.address=address;
     }
     void patientDetails(){
         System.out.println("Patient Details:\n");
         System.out.println("ID: "+ID+"\t name: "+name+"\t email: "+email+"\tPhone Number: "+phoneNumber+"\n");
+    }
+    void addPatientMedicalReport(String record){
+        this.record=record;
+    }
+    void viewDoctorProfiles(){
+
+    }
+    void payBill(){
+        bill.payBill();
+    }
+    void bookAppointment(){
+
     }
 }
